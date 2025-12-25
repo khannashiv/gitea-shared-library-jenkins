cb132a78e299a89a165319f8e01b8168373560ba
Shiv <khannashiv94@gmail.com>
2025-12-25 17:35:42 +0530

    updating trivyScanScipt with loadScript function such that enclosing trivy.sh in single quote to treat it as string.

--- vars/trivyScanScript.groovy
+++ vars/trivyScanScript.groovy
@@ -1,4 +1,4 @@
 def updateVulnerabilityScan(Map config = [:]) {
-    loadScript(name: trivy.sh)
+    loadScript(name: 'trivy.sh')
     sh "./trivy.sh ${config.severity} ${config.exit-code} ${config.imageName}"
 }
