7f5441ec7a366ff56df2248c6a11ff85799eed86
Shiv <khannashiv94@gmail.com>
2025-12-25 17:50:31 +0530

    updating exit-code to exitCode

--- vars/trivyScanScript.groovy
+++ vars/trivyScanScript.groovy
@@ -1,4 +1,4 @@
 def updateVulnerabilityScan(Map config = [:]) {
     loadScript(name: 'trivy.sh')
-    sh "./trivy.sh ${config.severity} ${config.exit-code} ${config.imageName}"
+    sh "./trivy.sh ${config.severity} ${config.exitCode} ${config.imageName}"
 }
